b8babb2e08de78afdfb9ebbcf602b5806f9b3add
Nicolas <npsa1794@gmail.com>
2020-08-07 17:12:02 -0400

    Interfaz de la calculadora creada

--- calculator/src/app/app.module.ts
+++ calculator/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
